79bb1070108cd6514d31996aee97753237ed6690
Ariel Balita <ariel.a.balita@gmail.com>
2019-03-11 08:48:21 +0800

    update

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var serveStatic = require('serve-static');
 
 var port = process.env.PORT || 5000;
 var app = express();
-
+app.get("/test", (req,res)=>{
+    res.sendStatus(200)
+});
 app.use(serveStatic(__dirname + "/dist"));
 app.listen(port);
+
+module.exports = app
 
--- test/TestInit.js
+++ test/TestInit.js
@@ -5,15 +5,15 @@ process.env.NODE_ENV = 'test';
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../app');
+let server = require('../server.js');
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 describe('Test #1', () => {
-    it('it should GET Success Response', (done) => {
+    it('GET Success Response', (done) => {
       chai.request(server)
-          .get('/')
+          .get('/test')
           .end((err, res) => {
                 res.should.have.status(200);
             done();
